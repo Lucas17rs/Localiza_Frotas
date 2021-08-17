3ad09876097a4a9f6bc967295e5aa0ce40ace184
Lucas <lucasakajiri@hotmail.com>
2021-08-17 11:00:20 -0300

    Add project files.

---
+++ Localiza_Frotas/Controllers/SingletonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Localiza_Frotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SingletonController : ControllerBase
+    {
+    }
+}
